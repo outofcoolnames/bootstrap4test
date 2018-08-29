bff850cf5ec8061697cf248db1ad8908c03d50fc
Thomas Cournane <thomas.cournane@aspengrovesolutions.com>
2018-08-29 01:23:34 +0100

    Add options method to all pre-flight

--- AngularTest/AngularTest/Controllers/UserController.cs
+++ AngularTest/AngularTest/Controllers/UserController.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using AngularTest.Data;
 using AngularTest.Models;
@@ -20,6 +22,20 @@ namespace AngularTest.Controllers
         public IEnumerable<User> GetUsers()
         {
             return _userContext.Users.ToList();
+        }
+
+        /// <summary>
+        /// Identifies a list of HTTP methods accessible for this resource
+        /// </summary>
+        /// <returns>A http response containing a "Access-Control-Allow-Method" header</returns>
+        [Route("api/User")]
+        public HttpResponseMessage Options()
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Headers.Add("Access-Control-Allow-Origin", "*");
+            response.Headers.Add("Access-Control-Allow-Methods", "GET");
+
+            return response;
         }
     }
 }
